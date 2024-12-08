19626a71e02c1ac47987bbc0d3732379642b0016
SergejL-code <skylerus@mail.de>
2024-12-08 14:17:34 +0100

    commit text

--- 12_Arrays_06_12_2024/HomeWork_12/script.js
+++ 12_Arrays_06_12_2024/HomeWork_12/script.js
@@ -1,87 +1,93 @@
 //* Task 1/
 
+document.addEventListener("DOMContentLoaded", () => {
 
-// created an array of “names” with the elements
-const names = ["Marie", "Alex", "Elena", "Dimitri"];
-//created an array of “names” with the elements
-const ages = [22, 31, 45, 53];
+  // created an array of “names” with the elements
+  const names = ["Marie", "Alex", "Elena", "Dimitri"];
+  //created an array of “names” with the elements
+  const ages = [22, 31, 45, 53];
 
-const str = ["года", "год", "лет", "года"];
+  const str = ["года", "год", "лет", "года"];
 
-//created an empty “nameAge” array 
-const nameAge = [];
+  //created an empty “nameAge” array
+  const nameAge = [];
 
-//iterated an array of “names” 
-for (let i = 0; i < names.length; i++) {
+  //iterated an array of “names”
+  for (let i = 0; i < names.length; i++) {
+    //added elements from the arrays "str", "ages", "names" to the “nameAge” array
 
- //added elements from the arrays "str", "ages", "names" to the “nameAge” array
+    nameAge.push(names[i] + " " + ages[i] + " " + str[i]);
+  }
 
-  nameAge.push(names[i] + " " + ages[i] + " " + str[i]);
-}
-//output the array data "nameAge" to the console
-console.log(nameAge);
+  //output the array data "nameAge" to the console
+  console.log(nameAge);
 
+  document.getElementById("text_1").innerHTML = nameAge.join("<br>");
 
+  //* Task 2
 
-//* Task 2/
-//created an empty “nameAge” array
-const rev = [];
-//iterated an array of “namesAge” 
-for (let i = 0; i < nameAge.length; i++) {
-  // added elements from an array "nameAge" to the “rev ” array
-  rev.push(nameAge[i]);
+  //created an empty “rev” array
+  const rev = [];
+  //iterated an array of “namesAge”
+  for (let i = 0; i < nameAge.length; i++) {
+    // added elements from an array "nameAge" to the “rev ” array
+    rev.push(nameAge[i]);
+    console.log(rev);
+  }
+  //expanded the elements in the ‘rev’ array in reverse order using the ‘reverse’ method
+
+  rev.reverse();
+  //output the array data "rev" to the console
   console.log(rev);
-}
-//expanded the elements in the ‘rev’ array in reverse order using the ‘reverse’ method
+  document.getElementById("text_2").innerHTML = rev.join("<br>");
 
-rev.reverse(nameAge);
-//output the array data "rev" to the console
-console.log(rev);
+  //* Task 3/
 
-//* Task 3/
-//created an empty “countries” array 
-const countries = [];
-//added elements to the end of the array "countries" using the method push() 
-countries.push("France");
-countries.push("Germany");
-countries.push("Italy");
+  //created an empty “countries” array
+  const countries = [];
+  //added elements to the end of the array "countries" using the method push()
+  countries.push("France");
+  countries.push("Germany");
+  countries.push("Italy");
 
-console.log(countries);
-//method of "pop()"" removes an element from the end of the array and stores it in the 'country' variable.
-let country = countries.pop();
+  console.log(countries);
+  //method of "pop()"" removes an element from the end of the array and stores it in the 'country' variable.
+  let country = countries.pop();
 
-console.log(countries);
-console.log(country);
-//add an element to the beginning of the array"countries" using the "unshift()" method 
-countries.unshift(country);
-console.log(countries);
+  console.log(countries);
+  console.log(country);
+  //add an element to the beginning of the array"countries" using the "unshift()" method
+  countries.unshift(country);
+  console.log(countries);
 
+  document.getElementById("text_3").innerHTML = countries.join("<br>");
+  //* Task 4/
 
-//* Task 4/
-//created an array of “myNumbers” with the elements 
-const myNumbers = [1, 2, 3, 4, 5];
-//created an empty “quad” array
-let quad = [];
-m  = [];
-console.log("original array", myNumbers, "new array", quad);
-//iterated an array of “myNumbers” 
-for (let i = 0; i < myNumbers.length; i++) {
+  //created an array of “myNumbers” with the elements
+  const myNumbers = [1, 2, 3, 4, 5];
+  //created an empty “quad” array
+  let quad = [];
+  multi = [];
+  console.log("original array", myNumbers, "new array", quad);
+  //iterated an array of “myNumbers”
+  for (let i = 0; i < myNumbers.length; i++) {
+    //*1.
 
-//*1.
-//multiply each element by 2 and add it to the array "m"
- m.push(myNumbers[i] * 2);
+    //multiply each element by 2 and add it to the array "m"
+    multi.push(myNumbers[i] * 2);
+
+    //*2.
+
+    quad.push(Math.pow(myNumbers[i], 2));
+  }
+
+  console.log("Multiply  ", multi);
+
+  //output the array data original array to the console
+
+  console.log("original array", myNumbers);
+
+  //output the array data quad array to the console
+  console.log("square from the original array ", quad);
   
-//*2. 
-  
-  quad.push( Math.pow(myNumbers[i], 2));
-}
-
-//output the array data "m" to the console
-console.log("Multiply  ",m);
-
-//output the array data original array to the console
-
-console.log("original array", myNumbers);
-  
-//output the array data quad array to the console
-console.log("square from the original array ",quad);
+  document.getElementById("text_4").innerHTML = multi.join("<br>");})
