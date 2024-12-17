50c8603e23497da240aaef4c85eb0a30b484abcd
SergejL-code <skylerus@mail.de>
2024-12-17 23:10:27 +0100

    commit text

--- 19_Fetch_practice_17_12_2024/HomeWork_19/script_hw19.js
+++ 19_Fetch_practice_17_12_2024/HomeWork_19/script_hw19.js
@@ -1,52 +1,45 @@
 const gridProducts = document.querySelector("#grid-dieArtikel");
 
-
 frageDerArtikel();
-
 
 async function frageDerArtikel() {
   const res = await fetch("https://dummyjson.com/products");
   const data = await res.json();
   console.log(data);
   data.products.map((product) => {
-
-
     //*создаем карточку*/
     const card = document.createElement("div");
-   console.log(card);
+    console.log(card);
     card.className = "artikel-card";
 
     //*создаём заголовок*/
-    const überschrift = document.createElement("h4");
+    const überschrift = document.createElement("h3");
     überschrift.textContent = product.title;
     card;
 
     //*создаем картинку*/
-   const img =document.createElement('img');
-   img.src=product.images;
-   img.alt=product.title;
-   //Создаем обертку для картинки- для обрезания
-   const wrapper = document.createElement("div");
-   wrapper.className = "img-wrapper";
-   wrapper.append(img);
+    const img = document.createElement("img");
+    img.src = product.images;
+    img.alt = product.title;
+    //Создаем обертку для картинки- для обрезания
+    const wrapper = document.createElement("div");
+    wrapper.className = "img-wrapper";
+    wrapper.append(img);
 
-   //*создаем описание товара*/
+    //*создаем описание товара*/
 
-   const desc = document.createElement('details')
-   desc.textContent = product.description;
+    const desc = document.createElement("details");
+    desc.textContent = product.description;
+//*маркер для детального описания 
+    const summary = document.createElement("summary");
+    summary.textContent = product.brand;
+    desc.append(summary);
+    //*добавляем цену*/
+    const price = document.createElement("p");
+    price.textContent = `Preis: ${product.price} Euro `;
 
-  //const shadowRoot = document.querySelector('element-with-shadow-root').shadowRoot;
-  // const summaryElement = shadowRoot.querySelector('summary');
-  // summaryElement.textContent = 'product.title'
-// const  op= document.createElement('summary')
-// op.textContent = product.brand;
-
-   //*добавляем цену*/
-    const price =document.createElement('p')
-    price.textContent = `Price: ${product.price} Euro `
- 
     //* добавляем элементы
-    card.append(überschrift,wrapper,desc,price);
+    card.append(überschrift, wrapper, desc, price);
     gridProducts.append(card);
   });
 }
